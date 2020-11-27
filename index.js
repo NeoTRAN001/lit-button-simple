1908035e60b83a499fff9729861b24c7ff908312
NeoTRAN001 <neo_tran@outlook.com>
2020-11-26 23:20:44 -0600

    Feat: Default project

---
+++ index.js
@@ -0,0 +1,1 @@
+export { LitButtonSimple } from './src/LitButtonSimple.js';
---
+++ lit-button-simple.js
@@ -0,0 +1,3 @@
+import { LitButtonSimple } from './src/LitButtonSimple.js';
+
+window.customElements.define('lit-button-simple', LitButtonSimple);
