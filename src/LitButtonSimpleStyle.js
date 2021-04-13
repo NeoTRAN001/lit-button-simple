b576ccc5a06471aca3ab353693690dc6ef8b9a28
NeoTRAN001 <neo_tran@outlook.com>
2021-04-13 09:53:16 -0500

    feat: focus and hover in buttons

--- src/LitButtonSimpleStyle.js
+++ src/LitButtonSimpleStyle.js
@@ -1,6 +1,7 @@
 import { css } from 'lit-element';
 
 export const LitButtonSimpleStyle = css`
+
     .button {
         background: none;
         border: 2px solid;
@@ -11,11 +12,11 @@ export const LitButtonSimpleStyle = css`
         cursor: pointer;
     }
 
-    .button:hover {
+    .shadow:hover {
         box-shadow: 5px 5px 5px rgba(0,0,0,0.4);
     }
 
-    .button:focus {
+    .shadow:focus {
         box-shadow: 0px 0px 0px rgba(0,0,0,0.4);
     }
 
@@ -37,9 +38,25 @@ export const LitButtonSimpleStyle = css`
         background: #18B74D;
     }
 
+    .success:hover {
+        background: #20cb59;
+    }
+
+    .success:focus {
+        background: #0baa40;
+    }
+
     .warning {
         color: white;
         background: #B5B718;
+    }
+
+    .warning:hover {
+        background: #c4c61f;
+    }
+
+    .warning:focus {
+        background: #afb10f;
     }
 
     .danger {
@@ -47,8 +64,24 @@ export const LitButtonSimpleStyle = css`
         background: #B71835;
     }
 
+    .danger:hover {
+        background: #c21a39;
+    }
+
+    .danger:focus {
+        background: #ad102c;
+    }
+
     .info {
         color: white;
         background: #1874B7;
+    }
+
+    .info:hover {
+        background: #1b7cc2;
+    }
+
+    .info:focus {
+        background: #126cae;
     }
 `;
